a2e2cddf9ef34e9ceecf1371705dac01fee274f6
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-11-28 18:52:22 +0300

    Update anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matched_words = []  
+        for word in word_list:
+            if sorted(self.word) == sorted(word):
+                matched_words.append(word)
+
+        return matched_words
